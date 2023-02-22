6f6e44393e382cbb4b7cf657132b68f479a2af35
muh rifai <muhrifai554@gmail.com>
2023-02-22 15:09:14 +0700

    add

--- lib/view/expand.dart
+++ lib/view/expand.dart
@@ -1,42 +1,96 @@
 import 'package:flutter/material.dart';
 
-class ExpandScreen extends StatelessWidget {
+class ExpandScreen extends StatefulWidget {
   const ExpandScreen({Key? key}) : super(key: key);
 
   @override
+  State<ExpandScreen> createState() => _ExpandScreenSate();
+}
+
+class _ExpandScreenSate extends State<ExpandScreen> {
+  String? _selectedValue;
+
+  final List<String> _dropdownValues = ['London', 'Munich'];
+
+  final TextEditingController departureController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                leading: Icon(Icons.album),
-                title: Text('The Enchanted Nightingale'),
-                subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  TextButton(
-                    child: const Text('BUY TICKETS'),
-                    onPressed: () {/* ... */},
+    return Container(
+        margin: const EdgeInsets.only(left: 15, top: 10, right: 15),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Card(
+              child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FormField<String>(
+              builder: (FormFieldState<String> state) {
+                const Text('Select a departure city:');
+                return InputDecorator(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(12, 10, 20, 20),
+                      // labelText: "hi",
+                      // labelStyle: textStyle,
+                      // labelText: _dropdownValue == null
+                      //     ? 'Where are you from'
+                      //     : 'From',
+                      // errorText: _errorBank,
+                      errorStyle: const TextStyle(
+                          color: Colors.redAccent, fontSize: 16.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black12,
+                        fontFamily: "verdana_regular",
+                      ),
+                      hint: const Text(
+                        "Pilih Kota Keberangkatan",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                          fontFamily: "verdana_regular",
+                        ),
+                      ),
+                      value: _selectedValue,
+                      isExpanded: true,
+                      isDense: true,
+                      onChanged: (value) {},
+                      // onChanged: () {
+                      //   // setState(() {
+                      //   //   _bankChoose = newValue.bank_name;
+                      //   // });
+                      // },
+                      items: _dropdownValues
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              Text(value),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                  const SizedBox(width: 8),
-                  TextButton(
-                    child: const Text('LISTEN'),
-                    onPressed: () {/* ... */},
-                  ),
-                  const SizedBox(width: 8),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                );
+              },
+            ),
+          )),
+        ));
   }
+}
+
+class BankListDataModel {
+  String bank_name;
+  String bank_logo;
+  BankListDataModel(this.bank_name, this.bank_logo);
 }
--- lib/view/order.dart
+++ lib/view/order.dart
@@ -4,46 +4,87 @@ class OrderScreen extends StatefulWidget {
   const OrderScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrderScreen> createState() => _OrderScreenState();
+  State<OrderScreen> createState() => _OrderScreenSate();
 }
 
-class _OrderScreenState extends State<OrderScreen> {
+class _OrderScreenSate extends State<OrderScreen> {
   String? _selectedValue;
 
   final List<String> _dropdownValues = ['London', 'Munich'];
 
+  final TextEditingController departureController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Dropdown Example'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Select a departure city:'),
-            DropdownButton<String>(
-              value: _selectedValue,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedValue = newValue;
-                });
+    return Container(
+        margin: const EdgeInsets.only(left: 15, top: 10, right: 15),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Card(
+              child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FormField<String>(
+              builder: (FormFieldState<String> state) {
+                const Text('Select a departure city:');
+                return InputDecorator(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(12, 10, 20, 20),
+                      // labelText: "hi",
+                      // labelStyle: textStyle,
+                      // labelText: _dropdownValue == null
+                      //     ? 'Where are you from'
+                      //     : 'From',
+                      // errorText: _errorBank,
+                      errorStyle: const TextStyle(
+                          color: Colors.redAccent, fontSize: 16.0),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black12,
+                        fontFamily: "verdana_regular",
+                      ),
+                      hint: const Text(
+                        "Pilih Kota Keberangkatan",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                          fontFamily: "verdana_regular",
+                        ),
+                      ),
+                      value: _selectedValue,
+                      isExpanded: true,
+                      isDense: true,
+                      onChanged: (value) {},
+                      // onChanged: () {
+                      //   // setState(() {
+                      //   //   _bankChoose = newValue.bank_name;
+                      //   // });
+                      // },
+                      items: _dropdownValues
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SizedBox(
+                                width: 15,
+                              ),
+                              Text(value),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                );
               },
-              items: _dropdownValues
-                  .map<DropdownMenuItem<String>>(
-                      (String value) => DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          ))
-                  .toList(),
             ),
-            const SizedBox(height: 16),
-            if (_selectedValue != null) Text('You selected $_selectedValue.'),
-          ],
-        ),
-      ),
-    );
+          )),
+        ));
   }
 }
